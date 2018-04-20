92ba440a4b0e67cbdf8db0ab7183731801765451
Ray <ray_wzc@163.com>
2018-04-20 11:50:59 +0800

    init

--- Example/SGQRCode/SGViewController.m
+++ Example/SGQRCode/SGViewController.m
@@ -7,23 +7,112 @@
 //
 
 #import "SGViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import "QRCodeGenerateVC.h"
+#import "WBQRCodeScanningVC.h"
+#import "WCQRCodeScanningVC.h"
 
-@interface SGViewController ()
-
+@interface SGViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSArray *dataList;
 @end
 
 @implementation SGViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self configuration];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)configuration {
+    _dataList = @[@"生成二维码", @"WBQRCode (popVC 逻辑处理）", @"WCQRCode (popToRootVC 逻辑处理)"];
+    [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    _tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _dataList.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    cell.backgroundColor = [UIColor clearColor];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.textLabel.text = _dataList[indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        QRCodeGenerateVC *VC = [[QRCodeGenerateVC alloc] init];
+        [self.navigationController pushViewController:VC animated:YES];
+    }
+    
+    if (indexPath.row == 1) {
+        WBQRCodeScanningVC *WBVC = [[WBQRCodeScanningVC alloc] init];
+        [self QRCodeScanVC:WBVC];
+    }
+    
+    if (indexPath.row == 2) {
+        WCQRCodeScanningVC *WCVC = [[WCQRCodeScanningVC alloc] init];
+        [self QRCodeScanVC:WCVC];
+    }
+}
+
+- (void)QRCodeScanVC:(UIViewController *)scanVC {
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if (device) {
+        AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+        switch (status) {
+            case AVAuthorizationStatusNotDetermined: {
+                [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                    if (granted) {
+                        dispatch_sync(dispatch_get_main_queue(), ^{
+                            [self.navigationController pushViewController:scanVC animated:YES];
+                        });
+                        NSLog(@"用户第一次同意了访问相机权限 - - %@", [NSThread currentThread]);
+                    } else {
+                        NSLog(@"用户第一次拒绝了访问相机权限 - - %@", [NSThread currentThread]);
+                    }
+                }];
+                break;
+            }
+            case AVAuthorizationStatusAuthorized: {
+                [self.navigationController pushViewController:scanVC animated:YES];
+                break;
+            }
+            case AVAuthorizationStatusDenied: {
+                UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"请去-> [设置 - 隐私 - 相机 - SGQRCodeExample] 打开访问开关" preferredStyle:(UIAlertControllerStyleAlert)];
+                UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+                    
+                }];
+                
+                [alertC addAction:alertA];
+                [self presentViewController:alertC animated:YES completion:nil];
+                break;
+            }
+            case AVAuthorizationStatusRestricted: {
+                NSLog(@"因为系统原因, 无法访问相册");
+                break;
+            }
+                
+            default:
+                break;
+        }
+        return;
+    }
+    
+    UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"未检测到您的摄像头" preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    
+    [alertC addAction:alertA];
+    [self presentViewController:alertC animated:YES completion:nil];
+}
+
 
 @end
+
+
--- SGQRCode.podspec
+++ SGQRCode.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'SGQRCode'
-  s.version          = '0.1.0'
+  s.version          = '2.2.1'
   s.summary          = 'A short description of SGQRCode.'
 
 # This description is used to generate tags and improve search results.
@@ -30,11 +30,11 @@ TODO: Add long description of the pod here.
 
   s.ios.deployment_target = '8.0'
 
-  s.source_files = 'SGQRCode/Classes/**/*'
+  s.source_files = 'SGQRCode/**/*'
   
-  # s.resource_bundles = {
-  #   'SGQRCode' => ['SGQRCode/Assets/*.png']
-  # }
+  s.resource_bundles = {
+    'SGQRCode' => ['SGQRCode/Assets/*']
+  }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
